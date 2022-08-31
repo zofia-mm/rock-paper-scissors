6b9f33ccad2f13eb846360b82a77bf9672650ad2
Fobya7 <66226017+Fobya7@users.noreply.github.com>
2022-08-31 20:47:15 +0200

    gave attribution component its own directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AttributionComponent } from './attribution.component';
+import { AttributionComponent } from './attribution/attribution.component';
 
 @NgModule({
   declarations: [
