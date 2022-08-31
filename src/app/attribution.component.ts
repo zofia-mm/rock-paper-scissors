8d66add69316146e8a62b7523b84527f30b4a04a
Fobya7 <66226017+Fobya7@users.noreply.github.com>
2022-08-31 20:33:53 +0200

    moved attribution into its own component

---
+++ src/app/attribution.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'attribution',
+    templateUrl: './attribution.component.html'
+    })
+export class AttributionComponent {}
