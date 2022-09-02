09f972124dbd4a7935123b1e7fe0e3e0db07a9b7
Fobya7 <66226017+Fobya7@users.noreply.github.com>
2022-09-02 23:48:37 +0200

    blocked out match

--- src/app/main/match/match.component.ts
+++ src/app/main/match/match.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { Handsign } from "../handsign/handsign.component";
 
 @Component({
     selector: 'match',
     templateUrl: './match.component.html',
     styleUrls: [
-        './match-layout.component.scss'
+        './match-layout.component.scss',
+        './match-typography.component.scss'
         ]
     })
-export class MatchComponent {}
+export class MatchComponent
+{
+    Handsign = Handsign;
+
+    @Input() did_win : boolean = false;
+    @Input() house_picked : boolean = false;
+    @Input() match_ended : boolean = false;
+
+    result_text = "You Win";
+}
+
+/*
+You Win
+You Lose
+*/
