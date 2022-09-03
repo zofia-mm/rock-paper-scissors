ae6d2276bd1c2c91b3f3ef0498ac78cdd0f18ba5
Fobya7 <66226017+Fobya7@users.noreply.github.com>
2022-09-03 20:05:33 +0200

    tweated match result

--- src/app/main/match/match-animations.component.ts
+++ src/app/main/match/match-animations.component.ts
@@ -45,36 +45,18 @@ export const expandBelowAnimation =
 [
     state(
         ResultAnimationState.Start,
-        style({
-            'height': '0',
-            'opacity' : 0
-            })
+        style({ 'opacity' : 0 })
         ),
     state(
         ResultAnimationState.End,
-        style({
-            'height': '8rem',
-            'opacity' : 1
-            })
+        style({ 'opacity' : 1 })
         ),
     
     transition( 'start => end',
     [
-        animate( '0.3s ease-out', keyframes([
-            style({
-                'height': '0',
-                }),
-            style({
-                'height': '8rem',
-                }),
-            ])),
-        animate( '0.8s', keyframes([
-            style({
-                'opacity': 0
-                }),
-            style({
-                'opacity': 1
-                })
+        animate( '0.3s', keyframes([
+            style({ 'opacity': 0 }),
+            style({ 'opacity': 1 })
             ]))
     ] )
 ];
